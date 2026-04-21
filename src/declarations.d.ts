60e40862bc075dc96ec15e1db9cc8aec3045563e
atmospherium <atmospherium@gmail.com>
2026-04-20 20:50:40 -0400

    Fix build issues

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,15 @@ declare module "*.txt" {
   const content: string;
   export default content;
 }
+
+// For side-effect imports like: import './style.css'
+declare module "*.css";
+
+// For SCSS if you are using it
+declare module "*.scss";
+
+// (Optional) For CSS Modules if you use: import styles from './App.module.css'
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
